2158117c7efb1884907cd0d409083700099de0bd
JoelKanyi <joelkanyi98@gmail.com>
2020-08-29 13:42:18 +0300

    worked on the customized toolbar

---
+++ app/src/main/java/com/kanyideveloper/gadsleaderboard/LeaderboardActivity.kt
@@ -0,0 +1,27 @@
+package com.kanyideveloper.gadsleaderboard
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.kanyideveloper.gadsleaderboard.ui.main.SectionsPagerAdapter
+
+class LeaderboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaderboard)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+    }
+
+
+}
