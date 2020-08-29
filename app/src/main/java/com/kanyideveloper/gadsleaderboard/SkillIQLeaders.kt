1f57bd43720aa599370d04e6138973cf4981ae06
JoelKanyi <joelkanyi98@gmail.com>
2020-08-29 14:10:21 +0300

    Added fragments

---
+++ app/src/main/java/com/kanyideveloper/gadsleaderboard/SkillIQLeaders.kt
@@ -0,0 +1,17 @@
+package com.kanyideveloper.gadsleaderboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SkillIQLeaders : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_skill_i_q_leaders, container, false)
+    }
+
+}
