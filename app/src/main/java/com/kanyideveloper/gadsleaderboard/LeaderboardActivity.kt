255f0b32cda1bc906d21d07274fbc037256c6ba5
JoelKanyi <joelkanyi98@gmail.com>
2020-08-29 15:13:00 +0300

    removed styles for v-21

--- app/src/main/java/com/kanyideveloper/gadsleaderboard/LeaderboardActivity.kt
+++ app/src/main/java/com/kanyideveloper/gadsleaderboard/LeaderboardActivity.kt
@@ -1,24 +1,26 @@
 package com.kanyideveloper.gadsleaderboard
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.kanyideveloper.gadsleaderboard.ui.main.SectionsPagerAdapter
+import androidx.appcompat.widget.Toolbar
+
 
 class LeaderboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leaderboard)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val adapter = ViewPagerAdapter(supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
+        adapter.addFragment(LearningLeaders(),"Learning Leaders")
+        adapter.addFragment(SkillIQLeaders(),"Skill IQ Leaders")
+        viewPager.adapter = adapter
         tabs.setupWithViewPager(viewPager)
 
     }
