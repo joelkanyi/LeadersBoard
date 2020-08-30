83c16499b6ee2d5c42233d29140645570feefee0
JoelKanyi <joelkanyi98@gmail.com>
2020-08-31 01:08:17 +0300

    removed extra layout and refactored main to splash

--- app/src/main/java/com/kanyideveloper/gadsleaderboard/SplashActivity.kt
+++ app/src/main/java/com/kanyideveloper/gadsleaderboard/SplashActivity.kt
@@ -3,10 +3,10 @@ package com.kanyideveloper.gadsleaderboard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
 
         //I will setup my splash screen
