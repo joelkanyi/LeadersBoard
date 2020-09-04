40d17439bf2de9b9ccd1053cf19c8c99396695be
JoelKanyi <joelkanyi98@gmail.com>
2020-09-04 20:52:00 +0300

    worked on the submission layout

---
+++ app/src/main/java/com/kanyideveloper/gadsleaderboard/ProjectSubmission.kt
@@ -0,0 +1,11 @@
+package com.kanyideveloper.gadsleaderboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProjectSubmission : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_submission)
+    }
+}
